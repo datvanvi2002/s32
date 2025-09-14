3d1d7a96280d059041fef550b40c9f66cef687cb
Dat <datvanvi.1407@gmail.com>
2025-09-15 00:15:05 +0700

    Project esp32 rust add wifi, rfid, matrix, oled

---
+++ readme.md
@@ -0,0 +1,3 @@
+1. cargo add esp-prinln
+in cargo.toml
+esp-println = { version = "0.12.0", features = ["esp32s3"] }
---
+++ src/main.rs
@@ -0,0 +1,366 @@
+use esp_idf_hal::spi::{config::Config as SpiConfig, SpiDeviceDriver, SpiDriver, SpiDriverConfig};
+use esp_idf_svc::hal::delay::Delay;
+use esp_idf_svc::hal::gpio::PinDriver;
+
+///add for wifi
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::nvs::{EspNvsPartition, NvsDefault};
+use esp_idf_svc::wifi::{AuthMethod, ClientConfiguration, Configuration, EspWifi};
+
+use esp_idf_hal::{gpio::*, peripherals::Peripherals};
+
+use esp_idf_sys as _;
+use mfrc522::comm::blocking::spi::SpiInterface;
+use mfrc522::Mfrc522;
+use std::{thread, time::Duration};
+
+use esp_idf_hal::i2c::{I2cConfig, I2cDriver};
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
+use ssd1306::{
+    mode::BufferedGraphicsMode, prelude::*, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306,
+};
+
+fn print_hex_bytes(data: &[u8]) {
+    for b in data {
+        print!("{:02x} ", b);
+    }
+    println!();
+}
+
+const SSID: &str = "SSID";
+const PASSWORD: &str = "PASS";
+
+fn main() -> anyhow::Result<()> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    unsafe {
+        let _ = esp_idf_sys::esp_task_wdt_deinit();
+    }
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap(); //lấy quyền truy cập các ngoại vi
+
+    //let pins = peripherals.pins; //,pin là tất cả các chân GPIO của chip
+    let keys = [
+        ['1', '2', '3', 'A'],
+        ['4', '5', '6', 'B'],
+        ['7', '8', '9', 'C'],
+        ['*', '0', '#', 'D'],
+    ];
+    let mut key_pressed: Option<char> = None;
+
+    let mut led = PinDriver::output(peripherals.pins.gpio2).unwrap(); // khởi tạo chân GPIO2 làm đầu ra
+
+    let mut row_0 = PinDriver::output(peripherals.pins.gpio9).unwrap();
+    let mut row_1 = PinDriver::output(peripherals.pins.gpio10).unwrap();
+    let mut row_2 = PinDriver::output(peripherals.pins.gpio11).unwrap();
+    let mut row_3 = PinDriver::output(peripherals.pins.gpio12).unwrap();
+
+    let mut col_0 = PinDriver::input(peripherals.pins.gpio13.downgrade()).unwrap();
+    col_0.set_pull(Pull::Up).unwrap();
+    let mut col_1 = PinDriver::input(peripherals.pins.gpio14.downgrade()).unwrap();
+    col_1.set_pull(Pull::Up).unwrap();
+    let mut col_2 = PinDriver::input(peripherals.pins.gpio21.downgrade()).unwrap();
+    col_2.set_pull(Pull::Up).unwrap();
+    let mut col_3 = PinDriver::input(peripherals.pins.gpio47.downgrade()).unwrap();
+    col_3.set_pull(Pull::Up).unwrap();
+
+    ////////////////INIT RFID MODULE/////////////////////
+    // Define GPIO pins for SPI
+    let sclk = peripherals.pins.gpio36; // SCLK pin
+    let mosi = peripherals.pins.gpio35; // MOSI pin
+    let mut cs = peripherals.pins.gpio48; //sda pin
+                                          // MISO is optional; set to None if not used
+    let miso = Some(peripherals.pins.gpio37); // MISO pin
+
+    // Add a reset pin (e.g., GPIO4; choose a free pin not conflicting with others)
+    let mut rst_pin = PinDriver::output(peripherals.pins.gpio39).unwrap();
+    // Perform hard reset RFID module
+    rst_pin.set_low().unwrap();
+    thread::sleep(Duration::from_millis(200));
+    rst_pin.set_high().unwrap();
+    thread::sleep(Duration::from_millis(200)); // Brief delay for stabilization
+                                               //Configure SPI driver
+    let spi_driver = SpiDriver::new(
+        peripherals.spi3, // Use SPI3
+        sclk,
+        mosi,
+        miso,
+        &SpiDriverConfig::new(),
+    )
+    .unwrap();
+
+    let config = SpiConfig::new()
+        .baudrate(1_000_000u32.into()) // 1 MHz
+        .data_mode(embedded_hal::spi::MODE_0);
+
+    let mut spi = SpiDeviceDriver::new(spi_driver, Some(cs), &config)?;
+
+    let delay = Delay::new(40000000); // Create a delay instance with a 240 MHz clock
+
+    thread::sleep(Duration::from_millis(1000));
+    let spi_interface = SpiInterface::new(spi);
+    let mut rfid = Mfrc522::new(spi_interface).init().unwrap();
+
+    thread::sleep(Duration::from_millis(1000));
+
+    let sector_num = 0; // Sector to read (0-15 for MIFARE Classic 1K)
+
+    /*Read version MFRC522*/
+    log::info!("Scanning for RFID card...");
+    let version = rfid
+        .version()
+        .map_err(|e| anyhow::anyhow!("Failed to read version: {:?}", e))?;
+    log::info!("MFRC522 Version: 0x{:02x}", version);
+
+    thread::sleep(Duration::from_millis(1000));
+
+    if rfid.reqa().is_err() {
+        log::warn!("RFID module may not be present or not responding!");
+    } else {
+        log::info!("RFID module initialized successfully!");
+    }
+
+    thread::sleep(Duration::from_millis(2000));
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    ///////////// INIT OLED MODULE////////////////////////////////////////////////////
+
+    log::warn!("Hello, world-- ESP32-PAYMENT-RUST!");
+
+    let sda = peripherals.pins.gpio8;
+    let scl = peripherals.pins.gpio18;
+
+    let i2c_cfg = I2cConfig::new();
+    let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &i2c_cfg)?;
+
+    // Tạo interface & driver SSD1306/SSD1309
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut disp: Ssd1306<_, DisplaySize128x64, BufferedGraphicsMode<_>> =
+        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+            .into_buffered_graphics_mode();
+    disp.init().unwrap();
+    disp.clear(BinaryColor::Off).unwrap();
+
+    let style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+    Text::new("hello", Point::new(0, 16), style)
+        .draw(&mut disp)
+        .unwrap();
+
+    disp.flush().unwrap();
+
+    /////////////////////////////////
+    log::info!("Init WIFI");
+    let modem = peripherals.modem;
+    let sysloop = EspSystemEventLoop::take()?;
+    let nvs_partition = EspNvsPartition::<NvsDefault>::take()?;
+    let mut wifi = EspWifi::new(modem, sysloop.clone(), Some(nvs_partition))?;
+
+    // Cấu hình STA
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        password: PASSWORD.try_into().unwrap(),
+        auth_method: AuthMethod::WPA2Personal, // chỉnh theo mạng
+        ..Default::default()
+    }))?;
+
+    // Start + connect
+    wifi.start()?;
+    wifi.connect()?;
+    log::info!("Đang kết nối tới Wi-Fi `{}` ...", SSID);
+
+    // Chờ kết nối & lấy IP (poll đơn giản)
+    let t0 = std::time::Instant::now();
+
+    loop {
+        //LED RUN STATUS
+        log::info!("Blinking LED 2...");
+        led.set_high().unwrap(); // bật đèn LED
+        thread::sleep(Duration::from_millis(1000)); // đợi 1 gi ây
+        led.set_low().unwrap(); // tắt đèn LED
+        thread::sleep(Duration::from_millis(1000));
+
+        /***********************************************************************************************************/
+        /********************************************** ĐỌC BÀN PHÍM MA TRẬN ***************************************/
+        /***********************************************************************************************************/
+
+        // Đọc bàn phím ma trận
+        log::info!("Reading keypad...");
+        key_pressed = None;
+        for i in 0..4 {
+            // Đặt tất cả hàng HIGH
+            row_0.set_high().unwrap();
+            row_1.set_high().unwrap();
+            row_2.set_high().unwrap();
+            row_3.set_high().unwrap();
+
+            // Đặt hàng hiện tại LOW
+            match i {
+                0 => row_0.set_low().unwrap(),
+                1 => row_1.set_low().unwrap(),
+                2 => row_2.set_low().unwrap(),
+                3 => row_3.set_low().unwrap(),
+                _ => {}
+            }
+            // Đọc trạng thái các cột
+            if col_0.is_low() {
+                //log::info!("Phím nhấn ở hàng {}, cột 0", i);
+                key_pressed = Some(keys[i][0]);
+                log::info!("Key pressed: {}", keys[i][0]);
+                break;
+            } else if col_1.is_low() {
+                //log::info!("Phím nhấn ở hàng {}, cột 1", i);
+                key_pressed = Some(keys[i][1]);
+                log::info!("Key pressed: {}", keys[i][1]);
+                break;
+            } else if col_2.is_low() {
+                //log::info!("Phím nhấn ở hàng {}, cột 2", i);
+                key_pressed = Some(keys[i][2]);
+                log::info!("Key pressed: {}", keys[i][2]);
+                break;
+            } else if col_3.is_low() {
+                //log::info!("Phím nhấn ở hàng {}, cột 3", i);
+                key_pressed = Some(keys[i][3]);
+                log::info!("Key pressed: {}", keys[i][3]);
+                break;
+            } else {
+                // key_pressed = None; // Không có phím nào được nhấn
+                log::info!("No key pressed in row {}", i);
+            }
+        }
+
+        /***********************************************************************************************************/
+        /********************************************** XỬ LÝ BÀN PHÍM MA TRẬN *************************************/
+        /***********************************************************************************************************/
+        if key_pressed == Some('A') {
+            led.toggle().unwrap(); // Bật LED nếu phím 'A' được nhấn
+        }
+
+        /***********************************************************************************************************/
+        /********************************************** xử lý rfid *************************************************/
+        /***********************************************************************************************************/
+        // nếu đang đọc thẻ mà rút thẻ ra thì sẽ bị reset
+        let mut uid_line_text = String::from("UID: -");
+        if let Ok(atqa) = rfid.reqa() {
+            log::info!("Card UID:  ");
+            thread::sleep(Duration::from_millis(500));
+
+            if let Ok(uid) = rfid.select(&atqa) {
+                print_hex_bytes(uid.as_bytes());
+                log::info!("{:02X?}", uid.as_bytes());
+                thread::sleep(Duration::from_millis(500));
+
+                log::info!("Reading sector: {:02X?}", sector_num);
+                read_sector(&uid, sector_num, &mut rfid);
+
+                rfid.hlta().unwrap();
+                rfid.stop_crypto1().unwrap();
+
+                let uid_bytes = uid.as_bytes();
+                let uid_str = format!("{:02X?}", uid_bytes);
+
+                uid_line_text = format!("UID: {}", uid_str);
+            }
+        } else {
+            log::info!("No card detected.");
+        }
+
+        /***********************************************************************************************************/
+        /********************************************** hiển thị OLED **********************************************/
+        /***********************************************************************************************************/
+
+        let key_text = match key_pressed {
+            Some(c) => format!("Key: {}", c),
+            None => "Key: -".to_string(),
+        };
+        //let ssid_text = format!("Wi-Fi: {}", SSID);
+
+        let ssid_text = match wifi.get_configuration() {
+            Ok(Configuration::Client(cfg)) => {
+                let ok = wifi.sta_netif().get_ip_info().is_ok(); // có IP => ok
+                format!(
+                    "Wi-Fi: {} [{}]",
+                    cfg.ssid.as_str(),
+                    if ok { "OK" } else { "N/A" }
+                )
+            }
+            _ => "Wi-Fi: - [N/A]".to_string(),
+        };
+
+        let y1 = 10; // dòng 1: SSID
+        let y2 = 24; // dòng 2: phím
+        let y3 = 38; // dòng 3: UID
+
+        {
+            use embedded_graphics::prelude::*;
+            use embedded_graphics::primitives::{PrimitiveStyle, Rectangle};
+
+            // mỗi dòng cao 12px
+            let h = 12u32;
+
+            // Clear line 1
+            Rectangle::new(Point::new(0, y1 - 10), Size::new(128, h))
+                .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off))
+                .draw(&mut disp)
+                .unwrap();
+
+            // Clear line 2
+            Rectangle::new(Point::new(0, y2 - 10), Size::new(128, h))
+                .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off))
+                .draw(&mut disp)
+                .unwrap();
+
+            // Clear line 3
+            Rectangle::new(Point::new(0, y3 - 10), Size::new(128, h))
+                .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off))
+                .draw(&mut disp)
+                .unwrap();
+        }
+
+        Text::new(&ssid_text, Point::new(0, y1), style)
+            .draw(&mut disp)
+            .unwrap();
+        Text::new(&key_text, Point::new(0, y2), style)
+            .draw(&mut disp)
+            .unwrap();
+        Text::new(&uid_line_text, Point::new(0, y3), style)
+            .draw(&mut disp)
+            .unwrap();
+
+        /***********************************************************************************************************/
+        /********************************************** Gửi thông tin lên server ***********************************/
+        /***********************************************************************************************************/
+
+        log::info!("From main.rs ESP32!");
+        thread::sleep(Duration::from_millis(200));
+    }
+}
+
+//read sector function
+fn read_sector<E, COMM: mfrc522::comm::Interface<Error = E>>(
+    uid: &mfrc522::Uid,
+    sector: u8,
+    rfid: &mut Mfrc522<COMM, mfrc522::Initialized>,
+) {
+    const AUTH_KEY: [u8; 6] = [0xFF; 6];
+
+    let block_offset = sector * 4;
+    rfid.mf_authenticate(uid, block_offset, &AUTH_KEY)
+        .map_err(|_| "Auth failed")
+        .unwrap();
+
+    for abs_block in block_offset..block_offset + 4 {
+        let data = rfid.mf_read(abs_block).map_err(|_| "Read failed").unwrap();
+        log::info!("{:02X?}", data);
+    }
+}
